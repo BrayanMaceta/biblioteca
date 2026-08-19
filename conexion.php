4c11bda6cc41083ca85f7571add46e1efcd775b7
BrayanMaceta <115665566+BrayanMaceta@users.noreply.github.com>
2026-08-19 18:08:59 -0500

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,14 @@
 <?php
+// Leer las variables de entorno que configuramos en Render
 $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
 $dbname = getenv('DB_NAME');
 
+// Crear la conexión con los datos de Clever Cloud
 $conexion = new mysqli($host, $user, $pass, $dbname);
+
+// Verificar si hubo error
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
