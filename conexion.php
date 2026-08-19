0ad3d05adc540e1ded2e0021f2f813228537fff6
BrayanMaceta <115665566+BrayanMaceta@users.noreply.github.com>
2026-08-19 17:09:15 -0500

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,23 +1,9 @@
 <?php
-// Leer las variables de entorno
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-
+$database_url = getenv('DATABASE_URL');
 try {
-    // Cadena de conexión forzando SSL
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
-    
-    // Crear la conexión
-    $conexion = new PDO($dsn, $user, $password);
-    
-    // Configurar errores
+    $conexion = new PDO($database_url);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
 } catch (PDOException $e) {
-    // Mostrar el error
     die("Error de conexión: " . $e->getMessage());
 }
 ?>
